d4ae8243cf82d0c761c37b31db805284e88ad403
sanskar00-debug <sanskarmuneshwar2004@gamil.com>
2026-07-08 19:59:35 +0530

    JavaScript is intialized

---
+++ script.js
@@ -0,0 +1,94 @@
+console.log("JavaScript is connected!")
+console.log("Hello Sanskar!")
+console.log(2 + 3)
+
+// Personal Details
+let myName = "Chikku"
+let myAge = 22
+let myCity = "Yavatmal"
+let isStudent = true
+
+console.log(myName)
+console.log(myAge)
+console.log(myCity)
+console.log(isStudent)
+
+
+
+// Text (String) - always in quotes
+let name = "Sanskar"
+let college = " Shri Shivaji's College of Engineering and Technology Akola"
+
+
+
+// Number -no quotes
+let age = 22
+let marks = 85.5
+
+
+
+// Boolean -only true or false
+let isLearning = true
+let isBored = false
+
+
+
+// Nothing
+let empty = null
+
+// Math in JavaScript
+let a = 10
+let b = 3
+
+console.log(a + b)    // 13
+console.log(a - b)    // 7
+console.log(a * b)    // 30
+console.log(a / b)    // 1 (remainder!)
+
+
+
+// Joining Words
+let firstName = "Sanskar"
+let lastName  = "Muneshwar"
+ 
+console.log(`My name is ${firstName} ${lastName}`)
+console.log(`I am ${myAge} year old from ${myCity}`)
+
+// Math
+let currentYear = 2026
+let birthYear = currentYear - age
+console.log(`I was born in ${birthYear}`)
+
+// IF/ELSE - making decisions !
+let MyAge = 22
+
+if (MyAge >= 18) {
+    console.log("You are an adult! ")
+} else {
+    console.log("You are a minor ")
+}
+
+// Another example
+let Marks = 85
+
+if (Marks >= 90) {
+    console.log("Grade: A+  ")
+} else if (Marks >= 80) {
+    console.log("Grade: A  ")
+} else if (Marks >= 70) {
+    console.log("Grade: B  ")
+} else if (Marks >= 60) {
+    console.log("Grade: C  ")
+} else {
+    console.log("Grade: F  ")
+}
+
+// Your marks from your rael subject!
+let tocMarks = 50
+let ooadMarks = 52
+
+if (tocMarks >= 50) {
+    console.log(`TOC: Passed  (${tocMarks} marks)`)
+} else {
+    console.log(`TOC: Failed  (${tocMarks} marks)`)
+}
